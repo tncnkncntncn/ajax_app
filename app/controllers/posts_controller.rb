904428e0194cdfe8c9383620b5e0875f4a95ef3a
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-06 18:10:12 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,9 @@ class PostsController < ApplicationController
     @posts = Post.order(id: "DESC")
   end
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+    #8行目では、7行目で定義した変数postの値を、postというキーとセットでJavaScriptに送信している
   end
 
 end
