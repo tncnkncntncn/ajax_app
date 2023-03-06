c212bd71796625f8d2d5c81e5ca94c2782555276
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-06 19:54:17 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,23 +1,45 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   const submit = document.getElementById("submit");
   //リクエストを送信する処理
-  
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-
     const form = document.getElementById("form");
     const formData = new FormData(form);
-    //フォームの要素を取得 
+    //フォームの要素を取得
     const XHR = new XMLHttpRequest();
-    //非同期通信を行うためにXMLHttpRequestオブジェクトを生成
+    //非同期通信を行うためにXMLHttpRequesオブジェクトを生成
     XHR.open("POST", "/posts", true);
-    //リクエストを指定。
+    //リクエストを指定
     XHR.responseType = "json";
-    //レスポンスのデータを「JSON形式」で返す
+    //レスポンスのデータを『JSON』形式で返す。
     XHR.send(formData);
-    // send()メソッドとは、リクエストを送信するメソッド.フォームに入力された内容をサーバー側に送信
-
+    //sendメソッドとは、リクエストを送信するするメソッド。フォームに入力された内容をサーバー側に送信する。
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      //<挿入したい要素名>.insertAdjacentHTML(挿入したい位置,挿入したいHTML);    HTMLを挿入したい箇所に挿入するメソッド。第一引数に挿入したい位置、第二引数に挿入したいHTMLを記述
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
- };
- 
- window.addEventListener('load', post);
+};
+
+window.addEventListener('load', post);
